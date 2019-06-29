5d7da16fd9991edafbe6c1e399ffca4b5c504f75
kevav27 <38163635+kevav27@users.noreply.github.com>
2019-06-28 20:17:22 -0600

    Cambios respecto a la base de datos y conexión a la misma (en proceso)

---
+++ Models/IDataBaseMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    interface IDataBaseMethods
+    {
+        void addElement();
+        void editElement();
+        void findElement();
+    }
+}
