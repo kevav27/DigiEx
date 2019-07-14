60dac312ac61ba671dafdb010d8b38fd328ba5a0
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-07-14 02:08:09 -0600

    Agregue modelo de GrupoFamiliar, con todos los datos que incluye

--- Models/GrupoFamiliarModel.cs
+++ Models/GrupoFamiliarModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+namespace DigiEx.Models
 {
-	public Class1()
-	{
-		
-	}
+    //Contenido del primer forulario
+    //Todos los datos dentro de la tabla GrupoFamiliar de la base de datos
+    public class GrupoFamiliarModel
+    {
+        private string gfNombre { get; set; } //GFNombre en tabla GrupoFamiliar
+        private string gfRelacion { get; set; } //GFRelacion en tabla GrupoFamiliar
+        private string gfEscolaridad { get; set; } //GFEscolaridad en tabla GrupoFamiliar
+        private string gfOcupacion { get; set; } //GFOcupacion en tabla GrupoFamiliar
+        private string gfIngresos { get; set; }  //GFIngresos en tabla GrupoFamiliar
+
+        //falta agregar los queries dentro del constructor, agreagar cuando se conecte a la base de datos
+        public GrupoFamiliarModel()
+        {
+
+        }
+    }
+
 }
