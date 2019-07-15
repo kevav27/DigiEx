55d66cd19f51919a7ba0685e4b7cbb3e610fa176
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-07-15 00:15:48 -0600

    Agregue clase HIConsecuenciasConsumo relacionado a la tabla de su mismo nombre de la base de datos

---
+++ Models/HIConsecuenciaConsumoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    //Contenido encontrado en el segundo formulario
+    //Todos los datos estan en la tabla HIPatronConsumo
+    //Todos las variables con terminacion Desc pueden ser nulas dependiendo de la respuesta de su contra parte
+    public class HIConsecuenciaConsumoModel
+    { 
+        private string ccConsumoAfectado { get; set; }
+        private string ccConsumoAfectadoDesc { get; set; }
+        private string ccAntecedentesFamiliares { get; set; }
+        private string ccAntecedentesPersonales { get; set; }
+
+        public HIConsecuenciaConsumoModel()
+        {
+
+        }
+    }
+}
