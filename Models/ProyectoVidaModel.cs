bffdb28dbe990569b4330e9eddc8a0a15ce1c09e
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-07-23 00:29:59 -0600

    Agregue modelo ProyectoVidaModel en base a la tabla ProyectoVida

---
+++ Models/ProyectoVidaModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    //Contenido encontrado en el segundo formulario
+    //Todos los datos estan en la tabla ProyectoVidaSocial
+    public class ProyectoVidaModel
+    {
+        private string pvFortalezas { get; set; }
+        private string pvOportunidades { get; set; }
+        private string pvDebilidades { get; set; }
+        private string pvAmenazas { get; set; }
+        private string pvObjetivoVidaEgresar { get; set; }
+        private string pvMetas { get; set; }
+        private string pvTareas { get; set; }
+        private string pvSeguimientoTratamiento { get; set; }
+        private string pvTiempoLibre { get; set; }
+        private string pvAcuerdosUltimaSesion { get; set; }
+
+        public ProyectoVidaModel()
+        {
+
+        }
+    }
+}
