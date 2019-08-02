120ff2bdd9bb43b1afa1413b4cc487f6e5d6e659
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-02 14:38:48 -0600

    Agregue Preguntas para etapa 1 basada a la tabla del mismo nombre

---
+++ Models/PlanTratamientoPreguntasEtapa1Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    //Contenido encontrado en el cuarto formulario (plan tratamiento)
+    //Todos los datos estan en la tabla PlanTratamientoPreguntasEtapa1
+    public class PlanTratamientoPreguntasEtapa1Model
+    {
+        private string e1AdaptaGrupo { get; set; }
+        private string e1ConductaSituacionAdictiva { get; set; }
+        private string e1MotivadoAlCambio { get; set; }
+        private string e1FamiliaInvolucradaCambio { get; set; }
+        private string e1VinculosRedesApoyo { get; set; }
+
+        public PPlanTratamientoPreguntasEtapa1Model()
+        {
+
+        }
+    }
+}
