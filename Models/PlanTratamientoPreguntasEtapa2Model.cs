41d6b48c1634756a83a54b5c0f077c43c5487d22
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-02 14:44:13 -0600

    Agregue el model de Plan Tratamiento Preguntas Etapa 2 con base a la tabla de su mismo nombre

---
+++ Models/PlanTratamientoPreguntasEtapa2Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    //Contenido encontrado en el cuarto formulario (plan tratamiento)
+    //Todos los datos estan en la tabla PlanTratamientoPreguntasEtapa2
+    public class PlanTratamientoPreguntasEtapa2Model
+    {
+        private string e2ContactoFamiliarMejorado { get; set; }
+        private string e2CodependenciaResidente { get; set; }
+        private string e2MejoraComunicacion { get; set; }
+        private string e2MejoraResponsabilidades { get; set; }
+        private string e2MotivacionCambio { get; set; }
+
+        public PPlanTratamientoPreguntasEtapa2Model()
+        {
+
+        }
+    }
+}
