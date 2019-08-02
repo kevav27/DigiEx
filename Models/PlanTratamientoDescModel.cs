1010065dd9b9fa404ee6117648ca0ccc0ea304b6
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-02 14:30:57 -0600

    Agregue model de Plan Tratameinto Desc basado con la base de datos en la tabla PlanTratamientoDesc

---
+++ Models/PlanTratamientoDescModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    //Contenido encontrado en el ultimo formulario (plan tratamiento)
+    //Todos los datos estan en la tabla PlanTratamientoDesc
+    public class PlanTratamientoDescModel
+    {
+        private string ptObjetivo { get; set; }
+        private string ptEstrategias { get; set; }
+        private string ptMeta { get; set; }
+
+        public PlanTratamientoDescModel()
+        {
+
+        }
+    }
+}
