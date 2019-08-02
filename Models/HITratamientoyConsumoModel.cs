fd27afa66008f0fe4872b0c21fdc7ab94adb8df0
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-02 16:36:34 -0600

    commit prueba

---
+++ Models/HITratamientoyConsumoModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiEx.Models
+{
+    //Contenido encontrado en el segundo formulario
+    //Todos los datos estan en la tabla HITratameintoyConsumo
+    //Todos las variables con terminacion Desc pueden ser nulas dependiendo de la respuesta de su contra parte
+    public class HITratamientoyConsumoModel
+    {
+        private string tcRecibidoTratamiento { get; set; }
+        private string tcRecibidoTratamientoDesc { get; set; }
+        private string tcAtendieron { get; set; }
+        private string tcAtendieronDesc { get; set; }
+        private string tcCompletoTratamiento { get; set; }
+        private string tcCompletoTratamientoDesc { get; set; }
+        private string tcTratamientoAntes { get; set; }
+        private string tcConsultasDrogas { get; set; }
+        private string tcInterrumpidoConsumo { get; set; }
+        private string tcInterrumpidoConsumoDesc { get; set; }
+        private string tcMotivoInterrupcion { get; set; }
+        private string tcMotivoInterrupcionDesc { get; set; }
+        private string tcActitudPareja { get; set; }
+        private string tcActitudFamilia { get; set; }
+        private string tcTrabajo_CentroEducativo { get; set; }
+        private string tcTrabajo_CentroEducativoDesc { get; set; }
+        private string tcActitudCompaneros { get; set; }
+
+        //falta agregar los queries dentro del constructor, agreagar cuando se conecte a la base de datos
+        public HITratamientoyConsumoModel()
+        {
+
+        }
+
+
+    }
+}
