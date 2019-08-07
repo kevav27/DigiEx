a7ae40fc5b38662aac3ed8f2db4288c743a4688c
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-07 16:00:33 -0600

    ...

--- Models/HiTratamientoyConsumoModel.cs
+++ Models/HiTratamientoyConsumoModel.cs
@@ -10,28 +10,30 @@ namespace DigiEx.Models
     //Todos las variables con terminacion Desc pueden ser nulas dependiendo de la respuesta de su contra parte
     public class HITratamientoyConsumoModel
     {
-        private string tcRecibidoTratamiento { get; set; }
-        private string tcRecibidoTratamientoDesc { get; set; }
-        private string tcAtendieron { get; set; }
-        private string tcAtendieronDesc { get; set; }
-        private string tcCompletoTratamiento { get; set; }
-        private string tcCompletoTratamientoDesc { get; set; }
-        private string tcTratamientoAntes { get; set; }
-        private string tcConsultasDrogas { get; set; }
-        private string tcInterrumpidoConsumo { get; set; }
-        private string tcInterrumpidoConsumoDesc { get; set; }
-        private string tcMotivoInterrupcion { get; set; }
-        private string tcMotivoInterrupcionDesc { get; set; }
-        private string tcActitudPareja { get; set; }
-        private string tcActitudFamilia { get; set; }
-        private string tcTrabajo_CentroEducativo { get; set; }
-        private string tcTrabajo_CentroEducativoDesc { get; set; }
-        private string tcActitudCompaneros { get; set; }
+        public string tcRecibidoTratamiento { get; set; }
+        public string tcRecibidoTratamientoDesc { get; set; }
+        public string tcAtendieron { get; set; }
+        public string tcAtendieronDesc { get; set; }
+        public string tcCompletoTratamiento { get; set; }
+        public string tcCompletoTratamientoDesc { get; set; }
+        public string tcTratamientoAntes { get; set; }
+        public string tcConsultasDrogas { get; set; }
+        public string tcInterrumpidoConsumo { get; set; }
+        public string tcInterrumpidoConsumoDesc { get; set; }
+        public string tcMotivoInterrupcion { get; set; }
+        public string tcMotivoInterrupcionDesc { get; set; }
+        public string tcActitudPareja { get; set; }
+        public string tcActitudFamilia { get; set; }
+        public string tcTrabajo_CentroEducativo { get; set; }
+        public string tcTrabajo_CentroEducativoDesc { get; set; }
+        public string tcActitudCompaneros { get; set; }
 
         //falta agregar los queries dentro del constructor, agreagar cuando se conecte a la base de datos
         public HITratamientoyConsumoModel()
         {
 
         }
+
+
     }
 }
