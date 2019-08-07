818f041eb6586849c64eec2bf685aa66f500a031
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-07 17:16:15 -0600

    Controllers creados con sus respectivos Models

---
+++ Controllers/GrupoFamiliarController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using DigiEx.Models;
+
+namespace DigiEx.Controllers
+{
+
+    public class GrupoFamiliarController : IDataBaseMethods
+    {
+        public void addElement(object o)
+        {
+            GrupoFamiliarModel grupoFamiliar = (GrupoFamiliarModel)o;
+
+        }
+
+        public void editElement(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void fetchElement(string f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void findElement(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
