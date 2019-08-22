ad383a0dc267a333792d5f1f58bc6a44a32e22d5
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-22 10:53:30 -0600

    Añadir el query a plantratamientoetapa2 controller

--- Controllers/PlanTratamientoPreguntasEtapa2Controller.cs
+++ Controllers/PlanTratamientoPreguntasEtapa2Controller.cs
@@ -15,6 +15,28 @@ namespace DigiEx.Controllers
         {
             PlanTratamientoPreguntasEtapa2Model planTratamientoPreguntasEtapa2 = (PlanTratamientoPreguntasEtapa2Model)o;
 
+            try
+            {
+                string query = "INSERT INTO dbo.PlanTratamientoPreguntasEtapa2 VALUES (E2ContactoFamiliarMejorado = @e2ContactoFamiliarMejorado, " + 
+                    "E2CodependenciaResidente = @e2CodependenciaResidente, " + 
+                    "E2MejoraComunicacion = @e2MejoraComunicacion, " + 
+                    "E2MejoraResponsabilidades = @e2MejoraResponsabilidades, " + 
+                    "E2MotivacionCambio = @e2MotivacionCambio)";
+
+                SqlCommand addPlanTratamientoPreguntasE2 = new SqlCommand(query, DBConectionController.con);
+                addPlanTratamientoPreguntasE2.Parameters.AddWithValue("e2ContactoFamiliarMejorado", planTratamientoPreguntasEtapa2.e2ContactoFamiliarMejorado);
+                addPlanTratamientoPreguntasE2.Parameters.AddWithValue("e2CodependenciaResidente", planTratamientoPreguntasEtapa2.e2CodependenciaResidente);
+                addPlanTratamientoPreguntasE2.Parameters.AddWithValue("e2MejoraComunicacion", planTratamientoPreguntasEtapa2.e2MejoraComunicacion);
+                addPlanTratamientoPreguntasE2.Parameters.AddWithValue("e2MejoraResponsabilidades", planTratamientoPreguntasEtapa2.e2MejoraResponsabilidades);
+                addPlanTratamientoPreguntasE2.Parameters.AddWithValue("e2MotivacionCambio", planTratamientoPreguntasEtapa2.e2MotivacionCambio);
+
+                addPlanTratamientoPreguntasE2.ExecuteNonQuery();
+                DBConectionController.con.Close();
+            }catch (Exception e)
+            {
+                //Error
+            }
+
         }
 
         public void editElement(int id, object o)
