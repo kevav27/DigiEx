3bc4fde535e18a84eee3c56995a2e5019950475b
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-22 10:46:56 -0600

    añadir el query a plantratamientoetapa1 controller

--- Controllers/PlanTratamientoPreguntasEtapa1Controller.cs
+++ Controllers/PlanTratamientoPreguntasEtapa1Controller.cs
@@ -15,6 +15,29 @@ namespace DigiEx.Controllers
         {
             PlanTratamientoPreguntasEtapa1Model planTratamientoPreguntasEtapa1 = (PlanTratamientoPreguntasEtapa1Model)o;
 
+            try
+            {
+                DBConectionController.con.Open();
+
+                string query = "INSERT INTO dbo.PlanTratamientoPreguntasEtapa1 VALUES (E1AdaptaGrupo = @e1AdaptaGrupo, " + 
+                    "E1ConductaSituacionAdictiva = @e1ConductaSituacionAdictiva, " + 
+                    "E1MotivadoAlCambio = @e1MotivadoAlCambio, " + 
+                    "E1FamiliaInvolucradaCambio = @e1FamiliaInvolucradaCambio, " + 
+                    "E1VinculosRedesApoyo = @e1VinculosRedesApoyo)";
+
+                SqlCommand addPlanTratamientoPreguntasE1 = new SqlCommand(query, DBConectionController.con);
+                addPlanTratamientoPreguntasE1.Parameters.AddWithValue("e1AdaptaGrupo", planTratamientoPreguntasEtapa1.e1AdaptaGrupo);
+                addPlanTratamientoPreguntasE1.Parameters.AddWithValue("e1ConductaSituacionAdictiva", planTratamientoPreguntasEtapa1.e1ConductaSituacionAdictiva);
+                addPlanTratamientoPreguntasE1.Parameters.AddWithValue("e1MotivadoAlCambio", planTratamientoPreguntasEtapa1.e1MotivadoAlCambio);
+                addPlanTratamientoPreguntasE1.Parameters.AddWithValue("e1FamiliaInvolucradaCambio", planTratamientoPreguntasEtapa1.e1FamiliaInvolucradaCambio);
+                addPlanTratamientoPreguntasE1.Parameters.AddWithValue("e1VinculosRedesApoyo", planTratamientoPreguntasEtapa1.e1VinculosRedesApoyo);
+
+                addPlanTratamientoPreguntasE1.ExecuteNonQuery();
+                DBConectionController.con.Close();
+            }catch (Exception e)
+            {
+                //Error
+            }
         }
 
         public void editElement(int id, object o)
