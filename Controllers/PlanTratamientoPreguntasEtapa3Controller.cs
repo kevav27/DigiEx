b4db5daf3459533911916bc5b707ff808a0c12d9
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-22 11:02:03 -0600

    añadir el query a plantratamientoe3 controller

--- Controllers/PlanTratamientoPreguntasEtapa3Controller.cs
+++ Controllers/PlanTratamientoPreguntasEtapa3Controller.cs
@@ -15,6 +15,34 @@ namespace DigiEx.Controllers
         {
             PlanTratamientoPreguntasEtapa3Model planTratamientoPreguntasEtapa3 = (PlanTratamientoPreguntasEtapa3Model)o;
 
+            try
+            {
+                string query = "INSERT INTO dbo.PlanTratamientoPreguntasEtapa3 VALUES (E3SituacionAdiccion = @e3SituacionAdiccion, " + 
+                    "E3ExistenciaRedesApoyo = @e3ExistenciaRedesApoyo, " + 
+                    "E3ParticipacionGruposApoyo = @e3ParticipacionGruposApoyo, " + 
+                    "E3DesarrolloHabilidadesSociales = @e3DesarrolloHabilidadesSociales, " + 
+                    "E3UsuarioAutonomia = @e3UsuarioAutonomia, " + 
+                    "E3MotivacionCambio = @e3MotivacionCambio, " + 
+                    "E3ProyectoVida = @e3ProyectoVida, " + 
+                    "E3PLanEgreso = @e3Planegreso)";
+
+                SqlCommand addPlanTratamientoPreguntasE3 = new SqlCommand(query, DBConectionController.con);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3SituacionAdiccion", planTratamientoPreguntasEtapa3.e3SituacionAdiccion);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3ExistenciaRedesApoyo", planTratamientoPreguntasEtapa3.e3ExistenciaRedesApoyo);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3ParticipacionGruposApoyo", planTratamientoPreguntasEtapa3.e3ParticipacionGruposApoyo);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3DesarrolloHabilidadesSociales", planTratamientoPreguntasEtapa3.e3DesarrolloHabilidadesSociales);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3UsuarioAutonomia", planTratamientoPreguntasEtapa3.e3UsuarioAutonomia);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3MotivacionCambio", planTratamientoPreguntasEtapa3.e3MotivacionCambio);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3ProyectoVida", planTratamientoPreguntasEtapa3.e3ProyectoVida);
+                addPlanTratamientoPreguntasE3.Parameters.AddWithValue("e3Planegreso", planTratamientoPreguntasEtapa3.e3Planegreso);
+
+                addPlanTratamientoPreguntasE3.ExecuteNonQuery();
+                DBConectionController.con.Close();
+            }catch (Exception e)
+            {
+                //Error
+            }
+
         }
 
         public void editElement(int id, object o)
