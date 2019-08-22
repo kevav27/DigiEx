837b68d1fab7e0f8ce723e3f0fdf61f8b75cd503
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-22 10:39:13 -0600

    añadir el query a plantratamientodesc controller

--- Controllers/PlanTratamientoDescController.cs
+++ Controllers/PlanTratamientoDescController.cs
@@ -15,6 +15,26 @@ namespace DigiEx.Controllers
         {
             PlanTratamientoDescModel planTratamientoDesc = (PlanTratamientoDescModel)o;
 
+            try
+            {
+                DBConectionController.con.Open();
+
+                string query = "INSERT INTO dbo.PlanTratamientoDesc VALUES (PTObjetivo = @ptObjetivo, " + 
+                    "PTEstrategias = @ptEstrategias, " + 
+                    "PTMeta = @ptMeta)";
+
+                SqlCommand addPlanTratamientoDesc = new SqlCommand(query, DBConectionController.con);
+                addPlanTratamientoDesc.Parameters.AddWithValue("ptObjetivo", planTratamientoDesc.ptObjetivo);
+                addPlanTratamientoDesc.Parameters.AddWithValue("ptEstrategias", planTratamientoDesc.ptEstrategias);
+                addPlanTratamientoDesc.Parameters.AddWithValue("ptMeta", planTratamientoDesc.ptMeta);
+
+                addPlanTratamientoDesc.ExecuteNonQuery();
+                DBConectionController.con.Close();
+            }catch (Exception e)
+            {
+                //Error
+            } 
+
         }
 
         public void editElement(int id, object o)
