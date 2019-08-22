aad0c26048c02f78f53ed64d750efe8612ba5d72
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-08-22 09:48:26 -0600

    Añadir query a PatronConsumoController

--- Controllers/HIPatronConsumoController.cs
+++ Controllers/HIPatronConsumoController.cs
@@ -15,6 +15,39 @@ namespace DigiEx.Controllers
         {
             HIPatronConsumoModel patronConsumo = (HIPatronConsumoModel)o;
 
+            try
+            {
+                DBConectionController.conn.Open();
+
+                string query = "INSERT INTO dbo.HIPatronConsumo VALUES (PCLugarConsumo = @pcLugarConsumo, " + 
+                    "PCLugarConsumoDesc = @pcLugarConsumoDesc, " + 
+                    "PCQuienesConsumo = @pcQuienesConsumo, " + 
+                    "PCQuienesConsumoDesc = @pcQuienesConsumoDesc, " + 
+                    "PCMotivoConsumo = @pcMotivoConsumo, " + 
+                    "PCMotivoConsumoDesc = @pcMotivoConsumoDesc, " + 
+                    "PCDrogasConsumidas = @pcDrogasConsumidas, " + 
+                    "PCFrecuenciaConsumo = @pcFrecuenciaConsumo, " + 
+                    "PCNivelConsumo = @pcNivelConsumo, " + 
+                    "PCDrogasUsadasUltimamente = @pcDrogasUsdasUltimamente)";
+
+                SqlCommand addPatronConsumo = new SqlCommand(query, DBConectionController.conn);
+                addPatronConsumo.Parameters.AddWithValue("pcLugarConsumo", patronConsumo.pcLugarConsumo);
+                addPatronConsumo.Parameters.AddWithValue("pcLugarConsumoDesc", patronConsumo.pcLugarConsumoDesc);
+                addPatronConsumo.Parameters.AddWithValue("pcQuienesConsumo", patronConsumo.pcQuienesConsumo);
+                addPatronConsumo.Parameters.AddWithValue("pcQuienesConsumoDesc", patronConsumo.pcQuienesConsumoDesc);
+                addPatronConsumo.Parameters.AddWithValue("pcMotivoConsumo", patronConsumo.pcMotivoConsumo);
+                addPatronConsumo.Parameters.AddWithValue("pcMotivoConsumoDesc", patronConsumo.pcMotivoConsumoDesc);
+                addPatronConsumo.Parameters.AddWithValue("pcDrogasConsumidas", patronConsumo.pcDrogasConsumidas);
+                addPatronConsumo.Parameters.AddWithValue("pcFrecuenciaConsumo", patronConsumo.pcFrecuenciaConsumo);
+                addPatronConsumo.Parameters.AddWithValue("pcNivelConsumo", patronConsumo.pcNivelConsumo);
+                addPatronConsumo.Parameters.AddWithValue("pcDrogasUsdasUltimamente", patronConsumo.pcDrogasUsdasUltimamente);
+
+                addPatronConsumo.ExecuteNonQuery();
+                DBConectionController.con.Close();
+            }catch (Exception e)
+            {
+                //Error
+            }
         }
 
         public void editElement(int id, object o)
